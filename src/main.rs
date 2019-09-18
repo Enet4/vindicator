736d36a6eb0b5b8b3776084e44854066130d6951
Eduardo Pinho <enet4mikeenet@gmail.com>
2019-09-18 10:35:18 +0100

    Strongly typed fuser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 pub extern crate noisy_float;
+use std::fs::{read_to_string, File};
 use std::io::BufWriter;
-use std::fs::{File, read_to_string};
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -16,12 +16,12 @@ pub enum App {
         files: Vec<PathBuf>,
         /// Result fusion algorithm
         #[structopt(short = "f")]
-        fuser: String,
+        fuser: Fuser,
         /// The output's query name
-        #[structopt(short = "q", long = "qid", default_value="fusion")]
+        #[structopt(short = "q", long = "qid", default_value = "fusion")]
         qid: String,
         /// The output's run name
-        #[structopt(long = "runid", default_value="vindicated")]
+        #[structopt(long = "runid", default_value = "vindicated")]
         runid: String,
         /// Output file (print to stdout by default)
         #[structopt(parse(from_os_str), short = "o")]
@@ -29,11 +29,40 @@ pub enum App {
     },
 }
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq, StructOpt)]
+pub enum Fuser {
+    #[structopt(name = "combMAX", alias = "combmax")]
+    CombMax,
+    #[structopt(name = "combSUM", alias = "combsum")]
+    CombSum,
+    #[structopt(name = "combMNZ", alias = "combmnz")]
+    CombMnz,
+}
+
+impl std::str::FromStr for Fuser {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "combMAX" | "combmax" | "max" => Ok(Fuser::CombMax),
+            "combSUM" | "combsum" | "sum" => Ok(Fuser::CombMax),
+            "combMNZ" | "combmnz" | "mnz" => Ok(Fuser::CombMax),
+            _ => Err(format!("Unknown fusion algorithm `{}`", s)),
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = App::from_args();
 
     match app {
-        App::Merge { files, fuser, output, qid, runid } => {
+        App::Merge {
+            files,
+            fuser,
+            output,
+            qid,
+            runid,
+        } => {
             let files_data = files
                 .iter()
                 .map(read_to_string)
@@ -43,31 +72,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .map(|data| trec::parse_from_trec(data))
                 .collect::<Result<Vec<_>, _>>()?;
             let list: Vec<_> = entries.into_iter().flatten().collect();
-            if let Some(list) = match &*fuser {
-                "combMAX" | "combmax" | "max" => {
-                    Some(fuser::fuse_scored(list, fuser::comb_max))
-                },
-                "combSUM" | "combsum" | "sum" => {
-                    Some(fuser::fuse_scored(list, fuser::comb_sum))
-                },
-                "combMNZ" | "combmnz" | "mnz" => {
-                    Some(fuser::fuse_scored(list, fuser::comb_mnz))
-                },
-                _ => {
-                    eprintln!("Unknown fusion algorithm `{}`", fuser);
-                    std::process::exit(-2);
-                },
+            if let Some(list) = match fuser {
+                Fuser::CombMax => Some(fuser::fuse_scored(list, fuser::comb_max)),
+                Fuser::CombSum => Some(fuser::fuse_scored(list, fuser::comb_sum)),
+                Fuser::CombMnz => Some(fuser::fuse_scored(list, fuser::comb_mnz)),
             } {
                 // transform results into new list
-                let list = list.into_iter()
-                    .enumerate()
-                    .map(|(i, e)| trec::TrecEntry {
-                        qid: &qid,
-                        docno: *e.id(),
-                        rank: i as Rank,
-                        score: e.score(),
-                        runid: &runid,
-                    });
+                let list = list.into_iter().enumerate().map(|(i, e)| trec::TrecEntry {
+                    qid: &qid,
+                    docno: *e.id(),
+                    rank: i as Rank,
+                    score: e.score(),
+                    runid: &runid,
+                });
 
                 // create output stream
                 match output {
